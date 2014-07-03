16d98ccd25f45b979b6d5061bf064507f531108b
Xindong Ding <dxd.spirits@gmail.com>
2014-07-03 21:49:27 +0800

    add city page

--- staticfiles/javascripts/dashboard/views/city.js
+++ staticfiles/javascripts/dashboard/views/city.js
@@ -1,9 +1,36 @@
 define(['app'], function(App) {
     
     App.Pages.City = new (Amour.PageView.extend({
-        events: {},
-        initPage: function() {},
-        render: function() {}
+        events: {
+            'click .left-btn': 'openArticle',
+            'click .btn-play': 'play'
+        },
+        initPage: function() {
+            this.city = new Amour.Models.City();
+            this.listenTo(this.city, 'change', this.renderCity);
+        },
+        openArticle: function() {
+            this.$('>article').toggleClass('open');
+        },
+        play: function() {
+            App.router.goTo('Question', {
+                questions: this.city.get('questions')
+            });
+        },
+        renderCity: function() {
+            Amour.loadBgImage(this.$el, this.city.get('image'));
+            this.$('.title').html(this.city.get('name'));
+            this.$('.content').html(this.city.get('description'));
+        },
+        render: function() {
+            this.$('>article').removeClass('open');
+            if (this.options.city) {
+                this.city.set(this.options.city)
+            } else if (this.options.cityId) {
+                this.city.set('id', this.options.cityId);
+                this.city.fetch();
+            }
+        }
     }))({el: $('#view-city')});
     
 });
